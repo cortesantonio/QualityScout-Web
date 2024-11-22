c7a6d64e5c887705382c5867bba94bc97a1f9103
hamipriv <1cpriv8@gmail.com>
2024-11-21 21:55:06 -0300

    Editada las vistas

--- ScannerCC/obj/Debug/net6.0/QualityScout.AssemblyInfo.cs
+++ ScannerCC/obj/Debug/net6.0/QualityScout.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QualityScout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57163a68b902ac209b70e86170c2971b3b71d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ebefbe1600650c38cfed9938e6e63797c73a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("QualityScout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QualityScout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
